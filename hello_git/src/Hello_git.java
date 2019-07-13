90109c04374bc656b8b3ac1ad3e732505492611f
xiaoshi <1784803114@qq.com>
2019-07-13 16:55:45 +0800

    范冰冰的

--- hello_git/src/Hello_git.java
+++ hello_git/src/Hello_git.java
@@ -60,6 +60,7 @@ public class Hello_git {
         System.out.println("我是小鱼");
         System.out.println("我是小鱼1111");
         System.out.println("wo shig ciaoyueder ");
+        System.out.println("wohfahg");
 
         System.out.println("fdhgjsojgsojgosfjgdsojoijgosijgodsfjgoisfjoi");
     }
