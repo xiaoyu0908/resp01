cd7179af47153b18057730e789cd3775b5812c43
xiaoshi <1784803114@qq.com>
2019-07-13 15:30:58 +0800

    我是Git我提交信息了

---
+++ hello_git/src/Hello_git.java
@@ -0,0 +1,11 @@
+public class Hello_git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
